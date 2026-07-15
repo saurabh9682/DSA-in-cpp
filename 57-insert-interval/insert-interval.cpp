72a722cd1c189a09e1d7f345e6c4874501abcd18
saurabh9682 <saurabhgupta8746@gmail.com>
2026-07-16 02:30:01 +0530

    Time: 3 ms (30.75%) | Memory: 21.7 MB (48.74%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+
+
+        intervals.push_back(newInterval); //important line
+
+         sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>>ans;
+        //interval ke andar ghuso pahle wale
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        //traverse karo intervalo me
+        for(int i=1;i<intervals.size();i++){
+            //suppose 2nd waala
+            int currstart=intervals[i][0];
+            int currend=intervals[i][1];
+            //if overlap hai
+            if(end>=currstart){
+                end=max(end,currend);
+            }
+            //if overlap nahi hai tab
+            else{
+                ans.push_back({start,end});
+                start=currstart;
+                end=currend;
+            }
+        }
+        ans.push_back({start,end});
+        return ans;
+    }
+};
