2d3952aca8662697250f2423875e69af117d2328
saurabh9682 <saurabhgupta8746@gmail.com>
2026-07-19 18:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (47.07%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy(-1);
+        dummy.next=head;
+        ListNode* prev=&dummy;
+        ListNode*curr=head;
+        while(curr!=nullptr){
+            if(curr->val==val){
+                prev->next=curr->next;
+                curr=curr->next;
+            }
+            else{
+                prev=curr;
+                curr=curr->next;
+            }
+        }
+        return dummy.next;
+    }
+};
