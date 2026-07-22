f10aa7244804fa8af24d163cac4166aa42e81fe7
saurabh9682 <saurabhgupta8746@gmail.com>
2026-07-22 21:08:01 +0530

    Time: 3 ms (9.03%) | Memory: 10.1 MB (6.89%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        //har charchter traverse karvao
-        for(int i=0;i<s.size();i++){
-            //if opening bracket mila to push
-            if(s[i]=='('||s[i]=='{'||s[i]=='['){
-                st.push(s[i]);
-            }
-            //closeing bracket 
-            else{
-                //stack empty hain
-                if(st.empty()){
-                    return false;
-                }
-                if(s[i]==')'&&st.top()=='('){
-                    st.pop();
-                }
-                else if(s[i]=='}'&&st.top()=='{'){
-                    st.pop();
-                }
-                else if(s[i]==']'&&st.top()=='['){
-                    st.pop();
-                }
-                else{
-                    return false;
-                }
-            }
+// treverse each charachter
+    for(int i=0;i<s.size();i++){
+        char ch=s[i]; //current character
+        if (ch=='('){
+            st.push(')');
         }
-        return st.empty();
+        else if (ch=='{'){
+            st.push('}');
+        }
+        else if (ch=='['){
+            st.push(']');
+        }
+        else if(st.empty()|| st.top()!=ch){
+            return false;
+        }
+        else {
+            st.pop();
+        }
+
+    }
+        return st.empty();//agar last me empty hogaya to valid parenthesis hua
     }
 };
