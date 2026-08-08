cfde8141b7b7b48bcfac3ba905109278c88be000
saurabh9682 <saurabhgupta8746@gmail.com>
2026-08-09 02:23:49 +0530

    Time: 75 ms (34.41%) | Memory: 111.3 MB (45.19%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+
+
+        /*Har number ke liye check karo:
+
+Agar number already map me present hai → duplicate mil gaya → true.
+Nahi hai → map me insert kar do.*/
         unordered_map<int,int>mp;
 
             // Agar number pehle aa chuka hai
