b29ff806d3095845662c785209ccf69a43812e92
saurabh9682 <saurabhgupta8746@gmail.com>
2026-08-20 23:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (20.15%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+
+        /*1 → num par Binary Search karo.
+mid * mid check karo:
+== num → true
+< num → right jao → left = mid + 1
+> num → left jao → right = mid - 1
+Search khatam ho gaya aur exact square nahi mila → false.*/
+        if(num==1){
+            return true;
+        }
+        int left=1;
+        int right=num;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(mid==num/mid){
+                if(mid*mid==num){
+                    return true;
+                }
+            }
+            if(mid<num/mid){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return false;
+    }
+}
