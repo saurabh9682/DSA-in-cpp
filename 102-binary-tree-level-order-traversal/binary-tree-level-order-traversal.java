6a1ed092254f9ea59b8b2b21e744efcd6398f43e
saurabh9682 <saurabhgupta8746@gmail.com>
2026-09-03 02:22:11 +0530

    Time: 1 ms (95.80%) | Memory: 46.6 MB (80.39%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -0,0 +1,35 @@
+/*
+Root ko queue me push karo.
+Jab tak queue empty nahi hai:
+Current queue ka size lo → ye current level ke nodes hain.
+Itne nodes ko process karo.
+Har node ki value current level list me add karo.
+Uske left aur right child ko queue me push karo.
+Har level ki list ko answer me add karte jao.*/
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+            Queue<TreeNode>q=new LinkedList<>();
+                q.add(root);
+            while(!q.isEmpty()){
+                int size=q.size();
+                List<Integer>temp=new ArrayList<>();
+                while(size-->0){
+                    TreeNode node=q.remove();temp.add(node.val);
+                    if(node.left!=null){
+                        q.add(node.left);
+                    }
+                    if(node.right!=null){
+                        q.add(node.right);
+                    }
+                }
+                    ans.add(temp);
+
+            }
+        
+                return ans;
+    }
+}
